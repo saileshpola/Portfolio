7955650a77739ce7523372bc793e08f0fdc34b47
saileshpola <saileshn75@gmail.com>
2025-04-19 14:52:58 -0400

    added email using form

--- send_email.py
+++ send_email.py
@@ -1,20 +1,17 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "saileshn75@gmail.com"
-password = "qylr pppc gaqt pyty"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "saileshn75@gmail.com"
+    password = "qylr pppc gaqt pyty"
+    receiver = "saileshn75@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "saileshn75@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = '''\
-Subject: Hi!
-How are you?
-Bye!
-'''
-with smtplib.SMTP_SSL(host,port, context =context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+
